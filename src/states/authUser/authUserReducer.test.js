0c42a48becd3db1575547209f77147148c07ed16
Mayong Saputra <gilang46622@gmail.com>
2026-08-26 09:48:41 +0700

    test: simulate ci failure for screenshot

--- src/states/authUser/authUserReducer.test.js
+++ src/states/authUser/authUserReducer.test.js
@@ -21,7 +21,7 @@ describe('authUserReducer function', () => {
     const nextState = authUserReducer(initialState, action);
 
     // 3. Assert
-    expect(nextState).toBe(initialState);
+    expect(nextState).toBe('HARUS ERROR');
   });
 
   it('should return the authUser when given by SET_AUTH_USER action', () => {
